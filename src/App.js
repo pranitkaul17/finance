08f716eff76762da1f2718886fb185c6de0647fc
Pranit Kaul <kaulpranit03@gmail.com>
2023-11-24 15:48:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Div1 from './components/Div1';
+import Div2 from './components/Div2';
+import Div3 from './components/Div3';
+import Div4 from './components/Div4';
+import Div5 from './components/Div5';
+import Login from './components/Registering/Login';
+import SignUp from './components/Registering/SignUp';
+import Home from './components/HomePage/Home';
+import Transact from './components/TransactionHistory/Transact';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* All Div components are rendered on the main path */}
+          <Route path="/" element={<div>
+            <Div1 />
+            <Div2 />
+            <Div3 />
+            <Div4 />
+            <Div5 />
+          </div>} />
+
+          {/* The Login component is rendered when the /login path is matched */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/Sign" element={<SignUp />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Transact" element={<Transact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
